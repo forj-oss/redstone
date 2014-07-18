2d241a5307aa4dee3491cc5d924aa69b55ddb9ac
Isaias Piña <iparjona@gmail.com>
2014-07-18 10:55:46 -0500

    Refactor to fix bug on Geppetto
    Change-Id: I6a7d4be24617106c14aa3bfd50551461781afe3d

--- puppet/modules/exim_config/manifests/utils.pp
+++ puppet/modules/exim_config/manifests/utils.pp
@@ -13,7 +13,7 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 #
-class exim_config::aux (
+class exim_config::utils (
 ) {
     $id_array = split($::domain, '\.')
     $id = $id_array[0]
