dddabe0407d82b95a0c48ce49aa59724598ad791
Edward Raigosa <edward.raigosa@gmail.com>
2014-08-08 15:59:35 -0700

    user story #2191: give jenkins plugins providers option
    - This adds a providers option to jenkins plugins so that it
      can now download a plugin from url and install it.
    - It behaves just as before with default options, and option
      to specify an alternative jenkins plugin root and plugin folder.
    - You can now specify a different jenkins user / group
    - you can specify the download md5 when using the provider download.
    - You can also specify ensure property absent to remove plugins.
    
    Change-Id: Ieb3e30d3211fd87e688cf054c2b156a29754b25d

--- puppet/modules/jenkins_config/manifests/plugin.pp
+++ puppet/modules/jenkins_config/manifests/plugin.pp
@@ -20,54 +20,105 @@
 #
 
 define jenkins_config::plugin(
-  $version=0,
-  $update_site = 'http://updates.jenkins-ci.org'
+  $version           = 0,
+  $provider          = jenkins, # possible values jenkins , download
+  $download_url      = undef,
+  $download_md5      = undef,
+  $jenkins_user      = 'jenkins',
+  $jenkins_group     = 'jenkins',
+  $update_site       = 'http://updates.jenkins-ci.org',
+  $plugin_parent_dir = '/var/lib/jenkins',
+  $plugin_dir        = '/var/lib/jenkins/plugins',
+  $ensure            = present,
 ) {
   $plugin            = "${name}.hpi"
-  $plugin_dir        = '/var/lib/jenkins/plugins'
-  $plugin_parent_dir = '/var/lib/jenkins'
 
-  if ($version != 0) {
-    $base_url = "${update_site}/download/plugins/${name}/${version}"
-  }
-  else {
-    $base_url   = "${update_site}/latest"
-  }
-
-  if (!defined(File[$plugin_dir])) {
-    file {
-      [
-        $plugin_parent_dir,
-        $plugin_dir,
-      ]:
-        ensure  => directory,
-        owner   => 'jenkins',
-        group   => 'jenkins',
-        require => [Group['jenkins'], User['jenkins']],
+  if ( $ensure == present)
+  {
+    if (!defined(File[$plugin_dir])) {
+      file {
+        [
+          $plugin_parent_dir,
+          $plugin_dir,
+        ]:
+          ensure  => directory,
+          owner   => $jenkins_user,
+          group   => $jenkins_group,
+          require => [Group[$jenkins_group], User[$jenkins_user]],
+      }
     }
-  }
 
-  if (!defined(Group['jenkins'])) {
-    group { 'jenkins' :
-      ensure => present,
+    if (!defined(Group[$jenkins_group])) {
+      group { $jenkins_group :
+        ensure => present,
+      }
     }
-  }
 
-  if (!defined(User['jenkins'])) {
-    user { 'jenkins' :
-      ensure => present,
+    if (!defined(User[$jenkins_user])) {
+      user { $jenkins_user :
+        ensure => present,
+      }
     }
-  }
 
-  exec { "download-${name}" :
-    command  => "wget --no-check-certificate ${base_url}/${plugin}",
-    cwd      => $plugin_dir,
-    require  => File[$plugin_dir],
-    path     => ['/usr/bin', '/usr/sbin',],
-    user     => 'jenkins',
-    unless   => "test -f ${plugin_dir}/${name}.?pi",
-#    OpenStack modification: don't auto-restart jenkins so we can control
-#    outage timing better.
-    notify   => Service['jenkins'],
+    if ( $provider == jenkins )
+    {
+      if ($version != 0) {
+        $base_url = "${update_site}/download/plugins/${name}/${version}"
+      }
+      else {
+        $base_url   = "${update_site}/latest"
+      }
+      exec { "download-${name}" :
+        command  => "wget --no-check-certificate ${base_url}/${plugin}",
+        cwd      => $plugin_dir,
+        require  => File[$plugin_dir],
+        path     => ['/usr/bin', '/usr/sbin',],
+        user     => 'jenkins',
+        unless   => "test -f ${plugin_dir}/${name}.?pi",
+    #    OpenStack modification: don't auto-restart jenkins so we can control
+    #    outage timing better.
+        notify   => Service['jenkins'],
+      }
+    } elsif ($provider == download)
+    {
+      if ($download_md5 != undef)
+      {
+        downloader { $download_url:
+          ensure   => present,
+          path     => "${plugin_dir}/${plugin}",
+          md5      => $download_md5,
+          owner    => $jenkins_user,
+          group    => $jenkins_group,
+          mode     => 644,
+          replace  => true,
+          provider => url,
+          require  => File[$plugin_dir],
+          notify   => Service['jenkins'],
+        }
+      }else
+      {
+        downloader { $download_url:
+          ensure   => present,
+          path     => "${plugin_dir}/${plugin}",
+          owner    => $jenkins_user,
+          group    => $jenkins_group,
+          mode     => 644,
+          replace  => true,
+          provider => url,
+          require  => File[$plugin_dir],
+          notify   => Service['jenkins'],
+        }
+      }
+    } else
+    {
+      fail("provider ${provider} not supported, choose another provider like jenkins or download.")
+    }
+  } elsif ( $ensure == absent )
+  {
+    file { "${plugin_dir}/${plugin}":
+      ensure => absent,
+    }
+  } else {
+    fail(" ensure , ${ensure}, is not supported.")
   }
 }
