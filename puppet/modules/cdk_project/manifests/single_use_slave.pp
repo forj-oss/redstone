be100073264d3e36b9c5242584f68b0f1c583ef8
Oscar Romero <homeless@hp.com>
2014-09-18 20:39:31 -0500

    Adding single_use_slave manifest for nodepool slaves use.
    - this manifest is used for the images created by nodepool
      so they are configured with this manifest for first time.
    
    Change-Id: I2d87125898b747971081ada0bf9bc72178f3dd8d

---
+++ puppet/modules/cdk_project/manifests/single_use_slave.pp
@@ -0,0 +1,75 @@
+# == Class: cdk_project::single_use_slave
+# Copyright 2013 OpenStack Foundation.
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+#
+#
+class cdk_project::single_use_slave (
+  $certname = $::fqdn,
+  $install_users = false,
+  $install_resolv_conf = true,
+  $sudo = false,
+  $thin = true,
+  $python3 = false,
+  $include_pypy = false,
+  $automatic_upgrades = false,
+  $all_mysql_privs = false,
+  $enable_unbound = true,
+  $ssh_key = '',
+  $bare = false,
+  $user = true,
+  $do_fortify = false,
+){
+  class { 'openstack_project::template':
+    certname            => $certname,
+    automatic_upgrades  => $automatic_upgrades,
+    install_users       => false,
+    install_resolv_conf => $install_resolv_conf,
+    enable_unbound      => $enable_unbound,
+    iptables_rules4     =>
+      [
+        # Ports 69 and 6385 allow to allow ironic VM nodes to reach tftp and
+        # the ironic API from the neutron public net
+        '-p udp --dport 69 -s 172.24.4.0/24 -j ACCEPT',
+        '-p tcp --dport 6385 -s 172.24.4.0/24 -j ACCEPT',
+        # Ports 8000, 8003, 8004 from the devstack neutron public net to allow
+        # nova servers to reach heat-api-cfn, heat-api-cloudwatch, heat-api
+        '-p tcp --dport 8000 -s 172.24.4.0/24 -j ACCEPT',
+        '-p tcp --dport 8003 -s 172.24.4.0/24 -j ACCEPT',
+        '-p tcp --dport 8004 -s 172.24.4.0/24 -j ACCEPT',
+      ],
+  }
+  class { 'jenkins_config::slave':
+    ssh_key         => $ssh_key,
+    python3         => $python3,
+    sudo            => $sudo,
+    bare            => $bare,
+    user            => $user,
+    include_pypy    => $include_pypy,
+    do_fortify      => $do_fortify,
+  }
+
+  class { 'openstack_project::slave_common':
+    include_pypy    => $include_pypy,
+    sudo            => $sudo,
+  }
+
+  if (! $thin) {
+    class { 'openstack_project::thick_slave':
+      all_mysql_privs => $all_mysql_privs,
+    }
+  }
+
+}
